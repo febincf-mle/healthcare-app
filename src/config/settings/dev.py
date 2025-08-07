fd5e325087df9b3bc3ac8680a73bf79bbd0a8ebf
febincf-mle <febin.web3dev@gmail.com>
2025-08-07 23:13:22 +0530

    project structure created

---
+++ src/config/settings/dev.py
@@ -0,0 +1,14 @@
+from .base import *
+
+# Allow all hosts for development.
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
